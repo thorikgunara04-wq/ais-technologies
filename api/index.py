c3fecf03aa38e1a7a0913caa5fe545d57f70ef59
thorikgunara04-wq <thorikgunara04@gmail.com>
2026-04-04 16:50:41 +0700

    Update model version from gemini-2.0-flash to gemini-2.0-latest

--- api/index.py
+++ api/index.py
@@ -17,7 +17,7 @@ def chat():
         user_message = user_data.get('message', '')
 
         # Pakai model 1.5-flash
-        model = genai.GenerativeModel('gemini-2.0-flash')
+        model = genai.GenerativeModel('gemini-2.0-latest')
 
         web_content = (
             "AIS Technologies adalah pengembang sistem bisnis berbasis digital milik Bro Thorik. "
